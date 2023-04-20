8af66f09306bc4e967fe3f02cf6f22f8f1eed434
g1mendoza <g1mendoza@ucsd.edu>
2023-04-19 18:11:19 -0700

    Checkpoint – After fixing the reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedInPlace2(){
+    int[] input2= {1,2,3,4};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{4,3,2,1}, input2);
+  }
+
+  @Test
+  public void testReversedInPlace3(){
+    int[] input3= {1,0};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{0,1},input3);
+
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input2= {2,4,6};
+    assertArrayEquals(new int[]{6,4,2}, ArrayExamples.reversed(input2));
+  }
 }
